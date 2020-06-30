7479c1882cda298d4a558cdb8b3bda46e776bd28
yudegoma <65863674+yudegoma@users.noreply.github.com>
2020-06-30 16:05:34 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-daily_path = "./json/daily.json"
-weekly_path = "./json/weekly.json"
-monthly_path = "./json/monthly.json"
-min30_path = "./json/min30.json"
+daily_path = "./daily.json"
+weekly_path = "./weekly.json"
+monthly_path = "./monthly.json"
+min30_path = "./min30.json"
