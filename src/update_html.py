c9378822953db025871687aa39add3320cc853fa
yudegoma <i14yokoro@python-server.us-west1-b.c.ivory-mountain-273706.internal>
2020-06-30 18:50:43 +0900

    UPDATE ファイル構造

---
+++ src/update_html.py
@@ -0,0 +1,36 @@
+from utils import *
+
+
+def dict_to_html(rank: dict) -> str:
+    i = 0
+    result = ""
+    for uuid, value in rank.items():
+        i += 1
+        result += "{:>3}位: {:>16}: {}<br>\n".format(i, uuid_to_name(uuid), value)
+    return result
+
+
+path_w = 'index.html'
+html = """
+<html>
+<head>
+<meta charset="UTF-8">
+<title>整地ランキング</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/marx/2.0.4/marx.css">
+</head>
+
+<body>
+<main>
+  <h1>ここになんか書く</h1>
+  <p>""" + dict_to_html(daily_rank()) + """
+</p>
+</main>
+</body>
+</html>
+"""
+
+with open(path_w, mode='w') as f:
+    f.write(html)
+
+with open(path_w, mode='r') as f:
+    print(f.read())
