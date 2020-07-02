f7dbf9330263d425cb75a2277c90b4d6b9092029
yudegoma <65863674+yudegoma@users.noreply.github.com>
2020-07-03 00:41:40 +0900

    Update update_jsons.py

--- src/update_jsons.py
+++ src/update_jsons.py
@@ -3,7 +3,7 @@ import config
 from utils import *
 
 url = "https://w4.minecraftserver.jp/api/ranking"
-payload = {"type": "break", "duration": "daily"}    # 日間整地ランキング
+payload = {"type": "break", "duration": "daily"}  # 日間整地ランキング
 
 
 def update_ranking():
@@ -13,6 +13,7 @@ def update_ranking():
     if r_get.status_code != requests.codes.ok:
         return
     r_json = r_get.json()
+
     ranks = r_json["ranks"].copy()
     while len(r_json["ranks"]) > 99:
         next_p = {"type": "break", "offset": str(len(ranks) - 1), "duration": "daily"}
@@ -22,14 +23,14 @@ def update_ranking():
 
     result = {}
     for rank in ranks:
-        result[rank["player"]["uuid"].replace("-", "")] = int(rank["data"]["raw_data"])
+        result[rank["player"]["uuid"].replace("-", "")] = {"name": rank["player"]["name"], "data": int(rank["data"]["raw_data"])}
 
     diff_daily = sort_dict(sub_dict(result, b_daily_rank))  # 更新前との差分
-
     # 各ランキング更新
     write_file(config.daily_path, sort_dict(result))
     write_file(config.weekly_path, sort_dict(add_dict(weekly_rank(), diff_daily)))
     write_file(config.monthly_path, sort_dict(add_dict(monthly_rank(), diff_daily)))
     write_file(config.min30_path, diff_daily)
-    
+
+
 update_ranking()
