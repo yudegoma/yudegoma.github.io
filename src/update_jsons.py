e80a5a33c4b7e26dfeee2cc179bc18ab3d5fbfb2
yudegoma <65863674+yudegoma@users.noreply.github.com>
2020-07-03 02:00:04 +0900

    Update update_jsons.py

--- src/update_jsons.py
+++ src/update_jsons.py
@@ -25,11 +25,11 @@ def update_ranking():
     for rank in ranks:
         result[rank["player"]["uuid"].replace("-", "")] = {"name": rank["player"]["name"], "data": int(rank["data"]["raw_data"])}
 
-    diff_daily = sort_dict(sub_dict(result, b_daily_rank))  # 更新前との差分
+    diff_daily = sort_dict(sub_dict(result.copy(), b_daily_rank))  # 更新前との差分
     # 各ランキング更新
     write_file(config.daily_path, sort_dict(result))
-    write_file(config.weekly_path, sort_dict(add_dict(weekly_rank(), diff_daily)))
-    write_file(config.monthly_path, sort_dict(add_dict(monthly_rank(), diff_daily)))
+    write_file(config.weekly_path, sort_dict(add_dict(update_mcid(weekly_rank(), result), diff_daily)))
+    write_file(config.monthly_path, sort_dict(add_dict(update_mcid(monthly_rank(), result), diff_daily)))
     write_file(config.min30_path, diff_daily)
 
 
