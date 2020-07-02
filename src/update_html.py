e9518631ea345509b55827556d69e87e94b60f74
yudegoma <65863674+yudegoma@users.noreply.github.com>
2020-07-02 21:18:19 +0900

    Update update_html.py

--- src/update_html.py
+++ src/update_html.py
@@ -9,28 +9,27 @@ def dict_to_html(rank: dict) -> str:
         result += "{:>3}位: {:>16}: {}<br>\n".format(i, uuid_to_name(uuid), value)
     return result
 
+def create_html(contents: str) -> str:
+    html = """
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>整地ランキング</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/marx/2.0.4/marx.css">
+    </head>
 
-path_w = 'index.html'
-html = """
-<html>
-<head>
-<meta charset="UTF-8">
-<title>整地ランキング</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/marx/2.0.4/marx.css">
-</head>
+    <body>
+    <main>
+      <h1>30分整地量</h1>
+      <p>""" + contents + """
+    </p>
+    </main>
+    </body>
+    </html>
+    """
+    return 
 
-<body>
-<main>
-  <h1>30分整地量</h1>
-  <p>""" + dict_to_html(min30_rank()) + """
-</p>
-</main>
-</body>
-</html>
-"""
+min30 = 'index.html'
 
-with open(path_w, mode='w') as f:
-    f.write(html)
-
-with open(path_w, mode='r') as f:
-    print(f.read())
+with open(min30, mode='w') as f:
+    f.write(create_html(dict_to_html(min30_rank())))
